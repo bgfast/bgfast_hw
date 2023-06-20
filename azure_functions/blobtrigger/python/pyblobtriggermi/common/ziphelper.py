286d7bbcbc6b46061adf232226a5daa6d16f6c35
bgfast <brent.groom@live.com>
2023-06-20 17:57:36 -0600

    managed identity initial check-in

--- azure_functions/blobtrigger/python/pyblobtriggermi/common/ziphelper.py
+++ azure_functions/blobtrigger/python/pyblobtriggermi/common/ziphelper.py
@@ -7,43 +7,30 @@ import os
 import sys
 import zipfile
 from azure.storage.queue import QueueClient
+from azure.identity import DefaultAzureCredential
 
 import azure.functions as func
 
-#########################
-# Logging setup
-# Set up logging
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-# Create a console handler and set the level to DEBUG
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-# Create a formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# Add the formatter to the handler
-ch.setFormatter(formatter)
-# Add the handler to the logger
-logger.addHandler(ch)
-# Done with logging setup
-#########################
-
-def process_zip_file(myblob, dest_container_client):
-    logger.debug("Starting process_zip_file")
+version = "ZH 1.0.10"
+def process_zip_file(myblob, dest_container_client, dest_queue_client):
+    logging.info('Starting process_zip_file. version: ' + version)
     # Read the contents of the zip file
     data = myblob.read()
-    #zip_file = zipfile.ZipFile(BytesIO(myblob))
     zip_file = zipfile.ZipFile(BytesIO(data))
-    connect_str = os.getenv('A1_AZURE_STORAGE_CONNECTION_STRING')
-    queue_name = os.getenv('A1_QUEUE_NAME')
 
-    #zip_file = zipfile.ZipFile(myblob)
     for file in zip_file.namelist():
-        #if file.endswith('.txt'):
-            # Write each file in the zip file as a new blob in the destination container
         with zip_file.open(file) as txt_file:
-            #contents = txt_file.read()
+            logging.info('Uploading file to blob '+txt_file.name+' version: ' + version)
             # only get the file name - ignore the path from inside the zip file
             blob_client = dest_container_client.get_blob_client(os.path.basename(txt_file.name))
             blob_client.upload_blob(txt_file.read(), overwrite=True)
-            queue_client = QueueClient.from_connection_string(conn_str=connect_str, queue_name=queue_name)
-            queue_client.send_message(os.path.basename(txt_file.name))
+            dest_queue_name = os.getenv('GENAI_DEST_QUEUE_NAME')
+            #connurl = os.getenv('AzureWebJobsStorage')
+            connurl = "hello"
+            logging.info('conn_str='+connurl +' version: ' + version)
+            #acctname = "bgfastfunczipsa"
+            #queue_client = QueueClient.from_connection_string(conn_str=connurl, queue_name=dest_queue_name,credential=DefaultAzureCredential())
+
+            #queue_client = QueueClient.from_connection_string(conn_str=connect_str, queue_name=queue_name)
+
+            #queue_client.send_message(os.path.basename(txt_file.name))
