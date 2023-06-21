1217f7469a205dd488c91b4c4283927ac1751357
bgfast <brent.groom@live.com>
2023-06-20 18:43:38 -0600

    initial project setup

--- azure_functions/httptrigger/python/pyhttptrigger/HttpTriggerWebPage/__init__.py
+++ azure_functions/httptrigger/python/pyhttptrigger/HttpTriggerWebPage/__init__.py
@@ -7,14 +7,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
